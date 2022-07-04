090c1c485799e634fe05cc03602819c4b71f9e03
Jeremy Raven <jeremyraven@icloud.com>
2022-07-04 16:12:38 +1200

    initialcommit

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-import {loadGLTF} from "/libs/loader.js";
+import {loadGLTF} from "./libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '/assets/targets/musicband.mind',
+      imageTargetSrc: './assets/targets/musicband.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const gltf = await loadGLTF('/assets/models/musicband-raccoon/scene.gltf');
+    const gltf = await loadGLTF('./assets/models/musicband-raccoon/scene.gltf');
     gltf.scene.scale.set(0.1, 0.1, 0.1);
     gltf.scene.position.set(0, -0.4, 0);
 
