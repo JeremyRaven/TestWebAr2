9750ed51424a097f61753c70ae617fe3e3684648
Jeremy Raven <jeremyraven@icloud.com>
2022-07-05 11:09:44 +1200

    initialcommit

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './assets/targets/musicband.mind',
+      imageTargetSrc: './assets/targets/Pylon10m.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
